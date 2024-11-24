4554b36ec988379bc7f4214e846de855a01bbd7b
Andres Morey <andresmarcel@gmail.com>
2024-11-24 23:24:26 +0300

    wip

--- kubetail.rb
+++ kubetail.rb
@@ -17,7 +17,7 @@ class Kubetail < Formula
   depends_on "pnpm" => :build
 
   def install
-    system "make", "build", "VERSION_FROM_URL=#{self.class.url}"
+    system "make", "build", "VERSION_FROM_URL=#{self.url}"
     bin.install "bin/kubetail"
     generate_completions_from_executable(bin/"kubetail", "completion")
   end
